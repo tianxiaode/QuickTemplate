232ff64ff9bc24a53e366af05b6b37502f6ca1b4
黄灯桥 <tianxiaode@hotmail.com>
2022-03-26 16:57:55 +0800

    20220326

--- src/client/workspace/packages/local/Common/src/view/identity/roles/Permissions.js
+++ src/client/workspace/packages/local/Common/src/view/identity/roles/Permissions.js
@@ -2,80 +2,135 @@ Ext.define('Common.view.identity.roles.Permissions',{
     extend: 'Ext.field.Field',
     xtype: 'uxpermissionsfield',
     
+    mixins:[
+        'Common.mixin.ConfigReady',
+    ],
+
     resourceName: 'Permissions',
     labelAlign: 'top',
     langLabel: 'Permissions',
     autoLabel: false,
 
+    permissions:{},
 
     checkBoxItem:  `<div class="x-checkcell" data-value="{0}">
             <span class="x-checkbox-el x-font-icon " ></span><span data-lang={2}>{1}</span>
     </div>`,
 
+    permissionsTpl:`
+        <div class='container'>
+            <tpl foreach=".">
+                <div class='row py-1'>
+                    <div class='col-auto text-truncate checkbox-label' style='width:140px;' data-lang='{$}'>{$}</div>
+                    <tpl foreach="item">
+                        <div class='col-auto px-2'>
+                            <div class="x-checkcell" data-value="{group}.{$}">
+                                <span class="x-checkbox-el x-font-icon" ></span><span data-lang='{parent.group}.{$}'>{parent.group}.{$}</span>
+                            </div>
+                        </div>
+                    </tpl>
+                </div>
+            </tpl>
+        </div>
+    `,
 
-    getBodyTemplate(){
+
+    onConfigReady(){
         let me = this,
-            policies = ACL.getPolicies(),
+            policies = Config.getAuthData().policies,
             resourceName = me.resourceName,
-            rows = {}, 
-            html = '';
+            permissions = me.permissions,
+            tpl = Ext.XTemplate.get(me.permissionsTpl);
         Ext.iterate(policies,p=>{
-            let permission = me.getPermission(p),
+            let permission = me.splitPermission(p),
                 key =permission.group + '.' + permission.default,
-                row = rows[key];
+                row = permissions[key];
             if(!row) {
-                row = rows[key] = [
-                    `<div class='py-1 row group' data-group='${key}'>` ,
-                    `<div class='col-auto text-truncate checkbox-label' style='width:140px;' data-lang='${key}'>${I18N.get(key, resourceName)}</div>`,
-                ];
-                let isSpecial = ['FeatureManagement','SettingManagement'].includes(permission.group);
-                isSpecial && row.push(me.getCheckBoxHtml(key, I18N.get(p, resourceName), p));
-                !isSpecial && row.push(me.getCheckBoxHtml(key, I18N.get('Permission:Access'), 'Permission:Access'));
+                row = permissions[key] = { group: key, item:{}};
+                // [
+                //     `<div class='py-1 row group' data-group='${key}'>` ,
+                //     `<div class='col-auto text-truncate checkbox-label' style='width:140px;' data-lang='${key}'>${I18N.get(key, resourceName)}</div>`,
+                // ];
+                // let isSpecial = ['FeatureManagement','SettingManagement'].includes(permission.group);
+                // isSpecial && row.push(me.getCheckBoxHtml(key, I18N.get(p, resourceName), p));
+                // !isSpecial && row.push(me.getCheckBoxHtml(key, I18N.get('Permission:Access'), 'Permission:Access'));
 
             }
             if(!Ext.isEmpty(permission.permission) && permission.permission !== 'ManagePermissions' ){
-                row.push(me.getCheckBoxHtml(p, I18N.get(p, resourceName), p));
+                row.item[permission.permission] = false;
+                //row.push(me.getCheckBoxHtml(p, I18N.get(p, resourceName), p));
             };
         });
-        Ext.iterate(rows,(r,v)=>{
-            html += v.join('') + '</div>';
-        })
-        return [{
-            reference: 'inputWrapElement',
-            cls: 'container',
-            html: html
-        }, {
-            reference: 'underlineElement',
-            cls: Ext.baseCSSPrefix + 'underline-el'
-        }];
+        me.bodyElement.setHtml(tpl.apply(permissions));
+        me.onLocalized();
     },
+
+    // getBodyTemplate(){
+    //     let me = this,
+    //         policies = ACL.getPolicies(),
+    //         resourceName = me.resourceName,
+    //         rows = {}, 
+    //         html = '';
+    //     Ext.iterate(policies,p=>{
+    //         let permission = me.getPermission(p),
+    //             key =permission.group + '.' + permission.default,
+    //             row = rows[key];
+    //         if(!row) {
+    //             row = rows[key] = { item:{}};
+    //             // [
+    //             //     `<div class='py-1 row group' data-group='${key}'>` ,
+    //             //     `<div class='col-auto text-truncate checkbox-label' style='width:140px;' data-lang='${key}'>${I18N.get(key, resourceName)}</div>`,
+    //             // ];
+    //             // let isSpecial = ['FeatureManagement','SettingManagement'].includes(permission.group);
+    //             // isSpecial && row.push(me.getCheckBoxHtml(key, I18N.get(p, resourceName), p));
+    //             // !isSpecial && row.push(me.getCheckBoxHtml(key, I18N.get('Permission:Access'), 'Permission:Access'));
+
+    //         }
+    //         if(!Ext.isEmpty(permission.permission) && permission.permission !== 'ManagePermissions' ){
+    //             row.item[permission.permission] = false;
+    //             //row.push(me.getCheckBoxHtml(p, I18N.get(p, resourceName), p));
+    //         };
+    //     });
+    //     console.log(rows);
+    //     // Ext.iterate(rows,(r,v)=>{
+    //     //     html += v.join('') + '</div>';
+    //     // })
+    //     return [{
+    //         reference: 'inputWrapElement',
+    //         cls: 'container',
+    //         html: html
+    //     }, {
+    //         reference: 'underlineElement',
+    //         cls: Ext.baseCSSPrefix + 'underline-el'
+    //     }];
+    // },
 
     getCheckBoxHtml(value,label, lang){
         let cb = Format.format(this.checkBoxItem,value,label,lang)
         return `<div class='col-auto px-2'>${cb}</div>`;
     },
 
-    updateValue(value, oldValue){
-        let me = this;
+    // updateValue(value, oldValue){
+    //     let me = this;
 
-        if (!(Ext.isEmpty(value) && Ext.isEmpty(oldValue))) {
-            me.validate();
-        }
-        console.log(value, oldValue);
-        let diff = Ext.Array.difference(value || [], oldValue || []);
-        console.log('diff', diff);
+    //     if (!(Ext.isEmpty(value) && Ext.isEmpty(oldValue))) {
+    //         me.validate();
+    //     }
+    //     console.log(value, oldValue);
+    //     let diff = Ext.Array.difference(value || [], oldValue || []);
+    //     console.log('diff', diff);
  
-        if (diff.length >0) {
-            me.initPermissions(value);
-            me._value = value;
+    //     if (diff.length >0) {
+    //         me.initPermissions(value);
+    //         me._value = value;
 
-            if (!me.isConfiguring) {
-                me.fireEvent('change', me, value, oldValue);
-            }
-        }
+    //         if (!me.isConfiguring) {
+    //             me.fireEvent('change', me, value, oldValue);
+    //         }
+    //     }
  
-        me.setDirty(me.isDirty());
-    },
+    //     me.setDirty(me.isDirty());
+    // },
 
     initPermissions(value){
         let me = this,
@@ -95,11 +150,11 @@ Ext.define('Common.view.identity.roles.Permissions',{
 
     },
 
-    initialize(){
-        let me = this;
-        me.callParent()
-        me.inputWrapElement.on('tap', me.PermissionCheck, me, { delegate :'.x-checkcell'})
-    },
+    // initialize(){
+    //     let me = this;
+    //     me.callParent()
+    //     me.inputWrapElement.on('tap', me.PermissionCheck, me, { delegate :'.x-checkcell'})
+    // },
 
     PermissionCheck(sender, target){
         let me = this,
@@ -107,12 +162,14 @@ Ext.define('Common.view.identity.roles.Permissions',{
             className = target.className,
             isCheck = !className.includes(checkCls),
             value = target.getAttribute('data-value'),
-            permission = me.getPermission(value),
-            oldValue = me.getValue() || [],
+            oldValue = Ext.clone(me.getValue()) || [],
             newValue = [];
         if(isCheck){
             oldValue.push(value);
+            let permission = me.getPermission(value);
+            //if(Ext.isEmpty(permission.permission)
             newValue = Ext.Array.clone(oldValue);
+            
         }else{
             newValue = Ext.Array.remove(oldValue, value);
         }
@@ -134,7 +191,7 @@ Ext.define('Common.view.identity.roles.Permissions',{
     onLocalized(){
         let me = this, 
             resourceName = me.resourceName,
-            el = me.inputWrapElement,
+            el = me.bodyElement,
             cbs = el.query('.x-checkcell'),
             cbLabels = el.query('.checkbox-label');
         Ext.each(cbs, cb=>{
@@ -148,7 +205,7 @@ Ext.define('Common.view.identity.roles.Permissions',{
         })
     },
 
-    getPermission(permission){
+    splitPermission(permission){
         let names = permission && permission.split('.');
         return {
             group: names[0],
