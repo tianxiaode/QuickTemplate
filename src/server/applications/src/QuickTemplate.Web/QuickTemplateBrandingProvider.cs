9fd715083da0d167e42c104632346a3c9d97f5f8
tianxiaode <tianxiaode@hotmail.com>
2023-08-24 22:28:52 +0800

    完成首页并更新网站

--- src/server/applications/src/QuickTemplate.Web/QuickTemplateBrandingProvider.cs
+++ src/server/applications/src/QuickTemplate.Web/QuickTemplateBrandingProvider.cs
@@ -9,10 +9,14 @@ namespace QuickTemplate.Web;
 public class QuickTemplateBrandingProvider : DefaultBrandingProvider
 {
     private readonly IStringLocalizer<QuickTemplateResource> _localizer;
+
     public QuickTemplateBrandingProvider(IStringLocalizer<QuickTemplateResource> localizer)
     {
         _localizer = localizer;
     }
 
     public override string AppName => _localizer["AppName"];
+
+    //public override string AppName => "";
+    public override string LogoUrl => "/images/logo.png";
 }
