5c9e83a915e2b195c079cb0e2165f46b13ef3697
tianxiaode <tianxiaode@hotmail.com>
2024-03-25 11:57:51 +0800

    修正config.js问题

--- src/client/workspace/packages/local/common-ux/development/config.js
+++ src/client/workspace/packages/local/common-ux/development/config.js
@@ -2,9 +2,9 @@ window.AppConfig = {
     currentLoadingText: '',
 
     //oidc设置
-    authority: 'https://localhost:44320/',
-    clientId: 'QuickTemplate_App',
-    scope: 'offline_access QuickTemplate',
+    authority: 'https://localhost:44329/',
+    clientId: 'TestReact_App',
+    scope: 'offline_access TestReact',
     responseType: 'code',
 
 
@@ -15,7 +15,7 @@ window.AppConfig = {
 
     //服务器设置
     //预防验证服务器与api服务器是分离的
-    server: 'https://localhost:44320/',
+    server: 'https://localhost:44380/',
     xsrfCookieName: 'XSRF-TOKEN',
     //xsrfHeaderName: 'X-XSRF-TOKEN',
     xsrfHeaderName: 'RequestVerificationToken',
@@ -25,7 +25,7 @@ window.AppConfig = {
 
     //应用设置
     loggerLevel: 'debug',
-    companyUrl: 'http://localhost:44320',
+    companyUrl: 'http://localhost:44384',
     copyrightStartValue: '2020',
     icp: '粤ICP备2022031812号',
     companyFullName: {
